5eefbdc678c2e208c4c658041ba23d191624729e
sz.sahaj <43649755+saszer@users.noreply.github.com>
2022-03-02 20:33:37 +1100

    InterfaceEditor

--- Assets/NFTPort SDK/Scripts/NFTPort_Interface.cs
+++ Assets/NFTPort SDK/Scripts/NFTPort_Interface.cs
@@ -10,14 +10,31 @@ namespace NFTPort
         typeof(Mint_Url)
 
     )]
-    public class NFTPort_Interface : Singleton<NFTPort_Interface>
+    
+    public class NFTPort_Interface : MonoBehaviour
     {
+        #region 
+        private static NFTPort_Interface _instance;
+        public static NFTPort_Interface Instance { get { return _instance; } }
+        
+        private void InterfaceInstance()
+        {
+            if (_instance != null && _instance != this)
+            {
+                Destroy(this.gameObject);
+            } else {
+                _instance = this;
+            }
+        }
+        #endregion
+      
         private NFTs_ownedbyanAccount _nfTsOwnedbyanAccount;
         private NFTs_fromAContract _nfTsFromAContract;
         private Mint_Url _mintUrl;
 
         private void Awake()
         {
+            InterfaceInstance();
             _nfTsOwnedbyanAccount = GetComponent<NFTs_ownedbyanAccount>();
             _nfTsFromAContract = GetComponent<NFTs_fromAContract>();
             _mintUrl = GetComponent<Mint_Url>();
