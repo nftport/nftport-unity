d33334e6d0a7b8d734ce5ef9b6485e69c60c3390
sz.sahaj <43649755+saszer@users.noreply.github.com>
2022-04-09 23:44:24 +1000

    refactor:: added base scripts to new structure,
    + Deleted old

---
+++ Runtime/Internal/Editor/NFTPortSettings.cs
@@ -0,0 +1,75 @@
+using System;
+using UnityEngine;
+using UnityEditor;
+using Newtonsoft.Json;
+
+namespace NFTPort.Editor
+{
+    [System.Serializable]
+    public class NFTPortSettings : EditorWindow
+    {
+        
+        public static string myAPIString = "Enter Your API Key";
+    
+        [MenuItem("NFTPort/Home")]
+        public static void ShowWindow()
+        {
+            GetWindow<NFTPortSettings>("Home");
+        }
+
+        void OnGUI()
+        {
+            Texture banner = Resources.Load<Texture>("banner");
+            GUILayout.Box(banner);
+        
+            GUILayout.Label("Welcome to NFTPort Unity SDK ", EditorStyles.boldLabel);
+
+            myAPIString = EditorGUILayout.TextField("APIKEY", myAPIString);
+            
+            if (GUILayout.Button("Save"))
+                SaveChanges();
+        }
+
+        void OnEnable()
+        {
+            ReadFromUserPrefs();
+        }
+
+        public override void SaveChanges()
+        {
+            WriteToUserPrefs();
+        }
+        
+
+        #region ReadWrite UserPrefs
+        
+        [SerializeField]
+        private Port.UserPrefs _userPrefs = new Port.UserPrefs();
+        void ReadFromUserPrefs()
+        {
+            TextAsset targetFile = Resources.Load<TextAsset>("NFTPort UserPrefs");;
+            if (targetFile != null)
+            {
+                _userPrefs = JsonConvert.DeserializeObject<Port.UserPrefs>(targetFile.text);
+                myAPIString = _userPrefs.API_KEY;
+            }
+            else
+            {
+                Port._initialised = false;
+                myAPIString = "Enter your API KEY here";
+            }
+                
+        }
+        void WriteToUserPrefs()
+        {
+            Port.SaveNewApi(myAPIString);
+            base.SaveChanges();
+        }
+
+        #endregion
+     
+        
+        
+    }
+}
+
---
+++ Runtime/Internal/ExtraResources/StringInListDrawer.cs
@@ -0,0 +1,50 @@
+using System;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+// from https://gist.github.com/ProGM/9cb9ae1f7c8c2a4bd3873e4df14a6687
+
+public class StringInList : PropertyAttribute {
+    public delegate string[] GetStringList();
+
+    public StringInList(params string [] list) {
+        List = list;
+    }
+
+    public StringInList(Type type, string methodName) {
+        var method = type.GetMethod (methodName);
+        if (method != null) {
+            List = method.Invoke (null, null) as string[];
+        } else {
+            Debug.LogError ("NO SUCH METHOD " + methodName + " FOR " + type);
+        }
+    }
+
+    public string[] List {
+        get;
+        private set;
+    }
+}
+
+#if UNITY_EDITOR
+[CustomPropertyDrawer(typeof(StringInList))]
+public class StringInListDrawer : PropertyDrawer {
+    // Draw the property inside the given rect
+    public override void OnGUI (Rect position, SerializedProperty property, GUIContent label) {
+        var stringInList = attribute as StringInList;
+        var list = stringInList.List;
+        if (property.propertyType == SerializedPropertyType.String) {
+            int index = Mathf.Max (0, Array.IndexOf (list, property.stringValue));
+            index = EditorGUI.Popup (position, property.displayName, index, list);
+
+            property.stringValue = list [index];
+        } else if (property.propertyType == SerializedPropertyType.Integer) {
+            property.intValue = EditorGUI.Popup (position, property.displayName, property.intValue, list);
+        } else {
+            base.OnGUI (position, property, label);
+        }
+    }
+}
+#endif
