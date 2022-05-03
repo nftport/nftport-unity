3586eb249217c7bd83326890d34bbd0748cd1fbb
sz.sahaj <43649755+saszer@users.noreply.github.com>
2022-05-03 22:54:18 +1000

    Upgrade to 2021 LTS + metamask connect fix

---
+++ Runtime/Internal/ConnectPlayerWallet.cs
@@ -0,0 +1,53 @@
+
+using System;
+
+namespace NFTPort
+{
+    using UnityEngine;
+    using UnityEngine.Events;
+    
+    public class ConnectPlayerWallet : MonoBehaviour
+    {
+        public string connectedWalletAddress;
+        
+        public UnityEvent afterSuccess;
+        private UnityAction<string> OnCompleteAction;
+
+        /// <summary>
+        /// Initialize creates a gameobject and assings this script as a component. This must be called if it doesn't already exists in the scene on gameObject named PlayerConnect_NFTPort.
+        /// </summary>
+        public static ConnectPlayerWallet Initialize()
+        {
+            var _this = new GameObject("PlayerConnect_NFTPort").AddComponent<ConnectPlayerWallet>();
+            return _this;
+        }
+        
+        /// <summary>
+        /// Action when player successfully connects the wallet.
+        /// </summary>
+        /// <returns> Player Wallet Address String </returns>
+        public ConnectPlayerWallet OnComplete(UnityAction<string> action)
+        {
+            this.OnCompleteAction = action;
+            return this;
+        }
+        
+        void Awake()
+        {
+            connectedWalletAddress = null;
+        }
+        
+        //called from index - For WebGL
+        public void WebHook_GetAddress(string recievedaddress)
+        {
+            connectedWalletAddress = recievedaddress;
+            Port.ConnectPlayerAddress = connectedWalletAddress;
+            
+            if(OnCompleteAction!=null)
+                OnCompleteAction.Invoke(connectedWalletAddress);
+                        
+            if(afterSuccess!=null)
+                afterSuccess.Invoke();
+        }
+    }
+}
---
+++ Samples/Metamask Connect - WebGL/PlayerConnect_Sample.cs
@@ -0,0 +1,29 @@
+namespace NFTPort.Samples.PlayerConnect
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using UnityEngine.UI;
+    using NFTPort;
+
+    public class PlayerConnect_Sample : MonoBehaviour
+    {
+        public Text addressText;
+
+        void Start()
+        {
+            ConnectPlayerWallet
+                .Initialize()
+                .OnComplete(address => UpdateUI(address));
+        }
+
+        void UpdateUI(string address)
+        {
+            addressText.text = address;
+            
+            //This value can also be accessed from anywhere globally via Port.
+            addressText.text = Port.ConnectPlayerAddress;
+        }
+        
+    }
+}
