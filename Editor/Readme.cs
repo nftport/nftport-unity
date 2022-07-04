e51027b62a487bea03ed19bfdcd2bd2049858de3
sz.sahaj <43649755+saszer@users.noreply.github.com>
2022-07-04 11:33:32 +1000

    fix: Custom dependency check and tool for Playground sample for unity Input System

---
+++ Editor/Readme.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+namespace NFTPort.Editor
+{
+	public class Readme : ScriptableObject {
+		public Texture2D icon;
+		public float iconMaxWidth = 128f;
+		public string title;
+		public Section[] sections;
+		public bool loadedLayout;
+		public bool notFirstload;
+	
+		[Serializable]
+		public class Section {
+			public string heading, text, linkText, url;
+		}
+	}
+}
