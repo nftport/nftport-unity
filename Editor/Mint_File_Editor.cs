8bdd1c0229eb98dbaeaaff2a5753d4b4bd7f0132
sz.sahaj <43649755+saszer@users.noreply.github.com>
2022-07-07 20:00:10 +1000

    feat: Easy Mint via File upload : Mint any in-game provedural NFTs to any wallet with single call.

---
+++ Editor/Mint_File_Editor.cs
@@ -0,0 +1,55 @@
+using System.IO;
+using UnityEngine;
+
+namespace NFTPort.Editor
+{
+    using UnityEditor;
+    using Internal;
+
+    [CustomEditor(typeof(Mint_File))]
+    public class Mint_File_Editor : Editor
+    {
+        private Mint_File myScript;
+        public override void OnInspectorGUI()
+        {
+            myScript = (Mint_File)target;
+            
+            
+            Texture banner = Resources.Load<Texture>("c_pminteasyFile");
+            GUILayout.BeginHorizontal();
+            GUILayout.Box(banner);
+            GUILayout.EndHorizontal();
+
+            if (GUILayout.Button("UPLOAD FILE & MINT", GUILayout.Height(45)))
+            {
+                PortUser.SetFromEditorWin();
+                myScript.Run();
+            }
+            if(GUILayout.Button("Stop Upload", GUILayout.Height(25)))
+                myScript.Stop(false);
+
+            if (GUILayout.Button("Select File", GUILayout.Height(35)))
+            {
+                OpenFile();
+            }
+            
+            if(GUILayout.Button("View Documentation", GUILayout.Height(25)))
+                Application.OpenURL(PortConstants.Docs_Mint_URL);
+
+            DrawDefaultInspector();
+        }
+        
+        public void OpenFile()
+        {
+            //Get the path
+            var path = EditorUtility.OpenFilePanel("Select File (⌐▨_▨) | NFTPort Upload", "", "*");
+            if (string.IsNullOrEmpty(path))
+                return;
+
+            //Read
+            var reader = new StreamReader(path);
+            myScript.SetParameters(FilePath:path);
+        }
+    }
+}
+
--- Runtime/Mint_File.cs
+++ Runtime/Mint_File.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Generic;
 using System.IO;
 using Newtonsoft.Json;
 using UnityEngine;
@@ -9,17 +10,32 @@ namespace NFTPort
 {
     using Internal;
     using Utils;
-    
-    [AddComponentMenu(PortConstants.BaseComponentMenu+PortConstants.FeatureName_StorageFiles)]
+
+    /// <summary>
+    /// Easy minting w/URL, If you wish to customize the minting process e.g. use your own contract, set more metadata, see Customizable minting.
+    /// </summary>
+    [AddComponentMenu(PortConstants.BaseComponentMenu + PortConstants.FeatureName_Mint_File)]
     [ExecuteAlways]
-    [HelpURL(PortConstants.Docs_StorageFile)]
-    public class Storage_UploadFile : MonoBehaviour
+    [HelpURL(PortConstants.Docs_Mint_File)]
+    public class Mint_File : MonoBehaviour
     {
+        public enum Chains
+        {
+            polygon,
+            rinkeby
+        }
+
         #region Parameter Defines
 
-        public string filePath;
-        [ReadOnly]public float uploadProgress = 0;
-        [ReadOnly]public float uplodedBytes = 0;
+        [ReadOnly] public float uploadProgress = 0;
+        [ReadOnly] public float uplodedBytes = 0;
+
+        [Space(20)] [SerializeField] private Chains _chain = Chains.polygon;
+
+        [SerializeField] private string _filePath = "Enter FilePath of the file to mint";
+        [SerializeField] private string _name = "Enter Name of the NFT";
+        [SerializeField] private string _description = "Enter Description of the NFT.";
+        [SerializeField] private string _mintToAddress = "Enter Blockchain address to mint to.";
 
         [Space(20)]
         //[Header("Called When API call starts")]
@@ -33,30 +49,33 @@ namespace NFTPort
 
         [Header("Run Component when this Game Object is Set Active")] [SerializeField]
         private bool onEnable = false;
+
         public bool debugErrorLog = true;
         public bool debugLogRawApiResponse = false;
 
-        [Header("Response after successful upload:")]
-        public Storage_model.Storage storageModel;
+        [Header("Response after successful mint:")]
+        public Minted_model minted;
 
         private UnityAction<string> OnStartedAction;
         private UnityAction<int> OnProgressAction;
         private UnityAction<string> OnErrorAction;
-        private UnityAction<Storage_model.Storage> OnCompleteAction;
-        private bool destroyAtEnd = false;
+        private UnityAction<Minted_model> OnCompleteAction;
 
-        private string RequestUriInit = "https://api.nftport.xyz/v0/files";
+        private string RequestUriInit = "https://api.nftport.xyz/v0/mints/easy/files";
         private string WEB_URL;
         private string _apiKey;
+        private bool destroyAtEnd = false;
 
         #endregion
+
 
         private void Awake()
         {
             PortUser.Initialise();
             _apiKey = PortUser.GetUserApiKey();
+
         }
-        
+
         private void OnEnable()
         {
             if (onEnable & Application.isPlaying)
@@ -65,90 +84,110 @@ namespace NFTPort
                 Run();
             }
         }
-        
+
         /// <summary>
         /// Initialize creates a gameobject and assings this script as a component. This must be called if you are not refrencing the script any other way and it doesn't already exists in the scene.
         /// </summary>
         /// <param name="destroyAtEnd"> Optional bool parameter can set to false to avoid Spawned GameObject being destroyed after the Api process is complete. </param>
-        public static Storage_UploadFile Initialize(bool destroyAtEnd = true)
+        public static Mint_File Initialize(bool destroyAtEnd = true)
         {
-            var _this = new GameObject("Storage_FileUpload").AddComponent<Storage_UploadFile>();
+            var _this = new GameObject(PortConstants.FeatureName_Mint_File).AddComponent<Mint_File>();
             _this.destroyAtEnd = destroyAtEnd;
             _this.onEnable = false;
             _this.debugErrorLog = false;
             return _this;
         }
 
-        public Storage_UploadFile SetFilePath(string _filePath)
+        /// <summary>
+        /// Set Easy Mint NFT Parameters
+        /// </summary>
+        /// <param name="FilePath"> URL of the file to mint.</param>
+        /// <param name="Name"> Name of the NFT.</param>
+        /// <param name="Description"> Description of the NFT. </param>
+        /// <param name="MintToAddress"> Blockchain address to mint to. </param>
+        public Mint_File SetParameters(string FilePath = null, string Name = null, string Description = null,
+            string MintToAddress = null)
         {
-            filePath = _filePath;
+            if (Name != null)
+                _name = Name;
+            if (Description != null)
+                _description = Description;
+            if (FilePath != null)
+                _filePath = FilePath;
+            if (MintToAddress != null)
+                _mintToAddress = MintToAddress;
             return this;
         }
-        
+
+        /// <summary>
+        /// Blockchain from which to query NFTs.
+        /// </summary>
+        /// <param name="chain"> Choose from available 'Chains' enum</param>
+        public Mint_File SetChain(Chains chain)
+        {
+            this._chain = chain;
+            return this;
+        }
+
         /// <summary>
         /// Action on File Upload Start ≧◔◡◔≦ .
         /// </summary>
         /// <param name="UnityAction action"> string.</param>
         /// <returns> string Sterted .</returns>
-        public Storage_UploadFile OnStarted(UnityAction<string> action)
+        public Mint_File OnStarted(UnityAction<string> action)
         {
             this.OnStartedAction = action;
             return this;
         }
-        
+
         /// <summary>
         /// Action on File Upload Progress returining Progress percentage
         /// </summary>
         /// <returns> float uploadProgress .</returns>
-        public Storage_UploadFile OnProgress(UnityAction<int> action)
+        public Mint_File OnProgress(UnityAction<int> action)
         {
             this.OnProgressAction = action;
             return this;
         }
 
         /// <summary>
-        /// Action on succesfull Upload ≧◉◡◉≦.
+        /// Action on succesfull API Fetch.
         /// </summary>
-        /// <param name="Storage_model.Storage"> Use: .OnComplete(_storageModel=> response = _storageModel) , where _storageModel = Storage_model.Storage;</param>
-        /// <returns> Storage_model</returns>
-        public Storage_UploadFile OnComplete(UnityAction<Storage_model.Storage> action)
+        /// <param name="Minted_model"> Use: .OnComplete(model=> Model = model) , where Model = Minted_model;</param>
+        /// <returns> Minted_model </returns>
+        public Mint_File OnComplete(UnityAction<Minted_model> action)
         {
             this.OnCompleteAction = action;
             return this;
         }
 
         /// <summary>
-        /// Action on Error (⊙.◎)
+        /// Action on Error
         /// </summary>
         /// <param name="UnityAction action"> string.</param>
         /// <returns> Information on Error as string text.</returns>
-        public Storage_UploadFile OnError(UnityAction<string> action)
+        public Mint_File OnError(UnityAction<string> action)
         {
             this.OnErrorAction = action;
             return this;
         }
 
         /// <summary>
-        /// Runs the API (ɔ◔‿◔)ɔ
+        /// Runs the Mint ^_^
         /// </summary>
-        public void Run()
+        public Minted_model Run()
         {
             WEB_URL = BuildUrl();
             StopAllCoroutines();
-            FileLocate(filePath);
+            FileLocate(_filePath);
+            return minted;
         }
-        
+
         public void Stop(bool destroy)
         {
             StopAllCoroutines();
             Debug.Log("File Upload Stopped");
             End();
-        }
-
-        string BuildUrl()
-        {
-            WEB_URL = RequestUriInit;
-            return WEB_URL;
         }
 
         void FileLocate(string path)
@@ -167,9 +206,28 @@ namespace NFTPort
             }
         }
 
+        private string RequestURL;
+
+        string BuildUrl()
+        {
+            https: //api.nftport.xyz/v0/mints/easy/files?chain=polygon&name=s&description=sss&mint_to_address=s
+            RequestURL = "?chain=" + _chain.ToString().ToLower() + "&name=" + _name + "&description=" + _description +
+                         "&mint_to_address=" + _mintToAddress;
+            WEB_URL = RequestUriInit + RequestURL;
+            return WEB_URL;
+        }
+
         private UnityWebRequest request;
+
         IEnumerator CallAPIProcess(string filePath)
         {
+            if (debugErrorLog)
+                Debug.Log("Mint  ⊂(▀¯▀⊂)   |  FILE");
+
+
+            if (debugErrorLog)
+                Debug.Log(RequestURL);
+
             var _FormParams = FormMaker.Formeet(filePath);
             request = new UnityWebRequest(WEB_URL, "POST");
             UploadHandler uploader = new UploadHandlerRaw(_FormParams.body);
@@ -180,15 +238,16 @@ namespace NFTPort
             request.SetRequestHeader("Authorization", _apiKey);
             request.SendWebRequest();
 
+
+            if (OnStartedAction != null)
+                OnStartedAction.Invoke("≧◔◡◔≦ Upload Started");
+            if (OnRequestStarted != null)
+                OnRequestStarted.Invoke();
+            if (debugErrorLog)
+                Debug.Log("≧◔◡◔≦ Upload Started");
+
             uploadProgress = 0;
             uplodedBytes = 0;
-            
-            if(OnStartedAction!=null)
-                OnStartedAction.Invoke("≧◔◡◔≦ Upload Started");
-            if(OnRequestStarted!=null)
-                OnRequestStarted.Invoke();
-            if(debugErrorLog)
-                Debug.Log("≧◔◡◔≦ Upload Started");
 
             bool uploaded = false;
             var lastprogress = uploadProgress;
@@ -197,7 +256,7 @@ namespace NFTPort
                 uploadProgress = request.uploadProgress * 100f;
                 uplodedBytes = request.uploadedBytes;
 
-                if ((int)lastprogress != (int)uploadProgress)
+                if ((int) lastprogress != (int) uploadProgress)
                 {
                     var uploadPerc = (int) (request.uploadProgress * 100f);
                     if (OnProgressAction != null)
@@ -206,50 +265,55 @@ namespace NFTPort
                     {
                         if (!uploaded)
                         {
-                            if(uploadPerc<100)
+                            if (uploadPerc < 100)
                                 Debug.Log("(ɔ◔‿◔)ɔ Uploading file. Progress " + uploadPerc + "%");
-                            else if(uploadPerc==100)
+                            else if (uploadPerc == 100)
                             {
-                                Debug.Log("≧◉◡◉≦ File Uploaded 100%, Awaiting API" );
+                                Debug.Log("≧◉◡◉≦ File Uploaded 100%, Awaiting API");
                                 uploaded = true;
                             }
                         }
                     }
+
                     lastprogress = uploadProgress;
                 }
+
                 yield return null;
             }
 
             string jsonResult = System.Text.Encoding.UTF8.GetString(request.downloadHandler.data);
-            
+
             if (debugLogRawApiResponse)
                 Debug.Log(jsonResult);
 
             if (request.error != null)
             {
                 if (OnErrorAction != null)
-                    OnErrorAction($"( ≖.≖) Response code: {request.responseCode}. Result {jsonResult}");
+                    OnErrorAction($"Null data. Response code: {request.responseCode}. Result {jsonResult}");
                 if (debugErrorLog)
-                    Debug.Log($"( ≖.≖) Response code: {request.responseCode}. Result {jsonResult}");
-                if(afterError!=null)
+                    Debug.Log($"(⊙.◎) Null data. Response code: {request.responseCode}. Result {jsonResult}");
+                if (afterError != null)
                     afterError.Invoke();
             }
             else
             {
                 //Fill Data Model from received class
-                storageModel = JsonConvert.DeserializeObject<Storage_model.Storage>(jsonResult);
+                minted = JsonConvert.DeserializeObject<Minted_model>(jsonResult);
 
                 if (OnCompleteAction != null)
-                    OnCompleteAction.Invoke(storageModel);
+                    OnCompleteAction.Invoke(minted);
+
                 if (afterSuccess != null)
                     afterSuccess.Invoke();
+
                 if (debugErrorLog)
-                    Debug.Log("(̶◉͛‿◉̶) Upload Done" + " at :" + storageModel.ipfs_url);
+                    Debug.Log($"NFTPort | Mint Success (⌐■_■) : at: {minted.transaction_external_url}");
             }
 
-         End();
+            End();
+
         }
-        
+
         void End()
         {
             if (request != null)
@@ -265,4 +329,3 @@ namespace NFTPort
         }
     }
 }
-
